dc77c248c61057eedea080d48dd85aa9628487ba
Muhammad Usaid <161123814+usaidkhan123@users.noreply.github.com>
2024-11-13 11:26:08 +0500

    Update about.tsx

--- app/components/Aboutsus/about.tsx
+++ app/components/Aboutsus/about.tsx
@@ -1,20 +1,15 @@
 const Aboutus = () => {
   return (
-    <div className="px-4 md:px-16 py-8  mb-24 ml-72">
-    
-      <h1 className="font-serif text-4xl sm:text-5xl md:text-6xl lg:text-8xl mt-8 sm:mt-12 md:mt-28">
-        Boost Your <br />
-        <span className="text-orange-500 -mb-12">Website</span> SEO
-      </h1>
+     <div className="px-4 sm:px-8 md:px-16 lg:px-24 py-8 mb-12 lg:mb-24">
+  <h1 className="font-serif text-3xl sm:text-4xl md:text-5xl lg:text-7xl xl:text-8xl mt-4 sm:mt-8 md:mt-12 lg:mt-16 text-center md:text-left">
+    Boost Your <br className="hidden md:block" />
+    <span className="text-orange-500">Website</span> SEO
+  </h1>
 
-    
-      <p className="mt-4 sm:mt-6 md:mt-8 font-mono text-base sm:text-lg md:text-sm -mb-12 text-gray-700">
-        is simply dummy text of the printing and typesetting industry. Lorem <br />
-        has been the industry s standard dummy text ever since the 1500s when <br />
-        specimen book. It has survived not only five centuries.
-      </p>
-
-    </div>
+  <p className="mt-4 sm:mt-6 md:mt-8 font-mono text-sm sm:text-base md:text-lg text-gray-700 leading-relaxed text-center md:text-left">
+    This is simply dummy text of the printing and typesetting industry. Lorem has been the industry’s standard dummy text ever since the 1500s when the specimen book was first created. It has survived not only five centuries but also the leap into electronic typesetting.
+  </p>
+</div>
   );
 };
 
